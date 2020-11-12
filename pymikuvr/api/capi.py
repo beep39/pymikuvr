2d0362c0c8122e67b0dd538bfc2ea8540729b736
beep39 <72642661+beep39@users.noreply.github.com>
2020-11-13 00:41:39 +0900

    dylib load fix

--- pymikuvr/api/capi.py
+++ pymikuvr/api/capi.py
@@ -3,12 +3,17 @@ import os.path
 import platform
 
 def load_lib():
-    lib_path = os.path.dirname(os.path.abspath(__file__)) + "/../../lib/"
+
+    lib_path = os.path.normpath(os.path.dirname(os.path.abspath(__file__)) + "/../../lib")
     system = platform.system()
     if system == 'Windows':
         os.add_dll_directory(lib_path)
         return ctypes.CDLL('pymikuvr.dll')
-    libname = "libpymikuvr.dylib" if system == 'Darwin' else "libpymikuvr.so"
-    return ctypes.CDLL(lib_path + libname)
+
+    path = os.getcwd()
+    os.chdir(lib_path)
+    lib = ctypes.CDLL("libpymikuvr.dylib" if system == 'Darwin' else "libpymikuvr.so")
+    os.chdir(path)
+    return lib
 
 c_lib = load_lib()
