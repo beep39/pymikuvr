74028005c5ef61ed177b6e8a91e830be59c33cfa
beep39 <72642661+beep39@users.noreply.github.com>
2021-03-15 14:47:38 +0900

    render.distance

--- pymikuvr/api/render.py
+++ pymikuvr/api/render.py
@@ -68,9 +68,8 @@ class shadows:
         self.__resolution = 0
         self._resolution = 4096
         self.__cascades = None
-        self._cascades = (4, 20, 70, 300)
         self.__bias = None
-        self._bias = ((3,8), (2,8), (2,8), (2,8))
+        self._bias = [(3,8), (2,8), (2,8), (2,8)]
 
     @property
     def enabled(self):
@@ -115,7 +114,7 @@ class shadows:
     def _bias(self):
         return self.__bias
 
-    @_cascades.setter
+    @_bias.setter
     def _bias(self, bias):
         for i in range(len(bias)):
             c_lib.render_set_shadows_bias(i, bias[i][0], bias[i][1])
@@ -134,10 +133,35 @@ class pipeline:
             c_lib.render_pipeline_set_condition(name.encode(), v)
 
 class render_class:
-    __slots__ = ('fps', '__light', '__shadows', '__pipeline')
+    __slots__ = ('fps', '__distance', '__light', '__shadows', '__pipeline')
     def __init__(self):
         self.fps = 0
+        self.__distance = 0
         self.reset()
+
+    @property
+    def distance(self):
+        return self.__distance
+
+    @distance.setter
+    def distance(self, distance):
+        self.__distance = distance
+
+        if distance <= 8:
+            self._set_znearfar(0.01, distance)
+            self.shadows._cascades = [distance]
+        elif distance <= 20:
+            self._set_znearfar(0.01, distance)
+            self.shadows._cascades = [4, distance]
+        elif distance <= 70:
+            self._set_znearfar(0.02, distance)
+            self.shadows._cascades = [4, 20, distance]
+        elif distance <= 300:
+            self._set_znearfar(0.05, distance)
+            self.shadows._cascades = [4, 20, 70, distance]
+        else:
+            self._set_znearfar(0.1, distance)
+            self.shadows._cascades = [distance * 0.015, distance * 0.07, distance * 0.3, distance]
 
     @property
     def light(self):
@@ -155,7 +179,7 @@ class render_class:
         self.__light = directional_light()
         self.__shadows = shadows()
         self.__pipeline = pipeline()
-        self._set_znearfar(0.05, 300)
+        self.distance = 300
 
     def _set_znearfar(self, znear, zfar):
         c_lib.render_set_znearfar(znear, zfar)
