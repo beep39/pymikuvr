a7770d561412c0ae87ec263c5776e2ef9f380bd5
beep39 <72642661+beep39@users.noreply.github.com>
2021-11-01 23:30:48 +0900

    linux fix

--- pymikuvr/api/capi.py
+++ pymikuvr/api/capi.py
@@ -3,17 +3,19 @@ import os.path
 import platform
 
 def load_lib():
-
     lib_path = os.path.normpath(os.path.dirname(os.path.abspath(__file__)) + "/../../lib")
     system = platform.system()
     if system == 'Windows':
         os.add_dll_directory(lib_path)
         return ctypes.CDLL('pymikuvr.dll')
 
-    path = os.getcwd()
-    os.chdir(lib_path)
-    lib = ctypes.CDLL("libpymikuvr.dylib" if system == 'Darwin' else "libpymikuvr.so")
-    os.chdir(path)
-    return lib
+    if system == 'Darwin':
+        path = os.getcwd()
+        os.chdir(lib_path)
+        lib = ctypes.CDLL("libpymikuvr.dylib")
+        os.chdir(path)
+        return lib
+
+    return ctypes.CDLL(os.path.join(lib_path, "libpymikuvr.so"))
 
 c_lib = load_lib()
